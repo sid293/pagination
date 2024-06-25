386902956d888d5ddc0f552861d57bef322187e3
Siddhesh Gawade <siddheshgawade61@gmail.com>
2024-06-25 16:35:49 +0530

    adding tbody and thead

--- src/App.js
+++ src/App.js
@@ -3,64 +3,66 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 function App() {
-  let [data , setData] = useState([]);
+  let [data, setData] = useState([]);
   let [filteredData, setFilteredData] = useState([]);
   let [currentPage, setCurrentPage] = useState(1);
 
-  let nextPage = ()=>{
-    setCurrentPage((prev)=> (((prev+1)*10)-10)<data.length?prev+1:prev);
+  let nextPage = () => {
+    setCurrentPage((prev) => (((prev + 1) * 10) - 10) < data.length ? prev + 1 : prev);
   };
-  let prevPage = ()=>{
-    setCurrentPage((prev)=> prev>1?prev-1:1);
+  let prevPage = () => {
+    setCurrentPage((prev) => prev > 1 ? prev - 1 : 1);
   };
 
-  function fillFiltersData(dataToFilter){
+  function fillFiltersData(dataToFilter) {
     let newArr = [];
-    for(let i=(currentPage*10)-10;i<(currentPage*10);i++){
-      if(i>=dataToFilter.length) break;
+    for (let i = (currentPage * 10) - 10; i < (currentPage * 10); i++) {
+      if (i >= dataToFilter.length) break;
       newArr.push(dataToFilter[i]);
     }
     setFilteredData(newArr);
   }
-  useEffect(()=>{
+  useEffect(() => {
     fillFiltersData(data);
-  },[currentPage, data]);
+  }, [currentPage, data]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      fillFiltersData(data);
-      setData(data)
-    })
-    .catch((err)=>{alert("failed to fetch data")});
-  },[]);
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        fillFiltersData(data);
+        setData(data)
+      })
+      .catch((err) => { alert("failed to fetch data") });
+  }, []);
 
 
   return (
     <div className="App">
       <h1>Employee Data Table</h1>
-      {filteredData.length!==0 && 
-      <table className="empTable" style={{width:"100vw",height:'70vh'}}>
-        <tr style={{backgroundColor:"green",color:"white"}}>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Role</th>
-        </tr>
-        {filteredData.map((fdata, index)=>(
-          <tr key={index}>
-            <td>{fdata.id}</td>
-            <td>{fdata.name}</td>
-            <td>{fdata.email}</td>
-            <td>{fdata.role}</td>
+      <table className="empTable" style={{ width: "100vw", height: '70vh' }}>
+        <thead style={{ backgroundColor: "green", color: "white" }}>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {filteredData.map((fdata, index) => (
+            <tr key={index}>
+              <td>{fdata.id}</td>
+              <td>{fdata.name}</td>
+              <td>{fdata.email}</td>
+              <td>{fdata.role}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-      }
-      <div style={{display:"flex",justifyContent:"center", gap:'8px'}} className="buttons">
+      <div style={{ display: "flex", justifyContent: "center", gap: '8px' }} className="buttons">
         <button onClick={prevPage}>Previous</button>
         <div className="currentPage">{currentPage}</div>
         <button onClick={nextPage}>Next</button>
