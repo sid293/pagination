1be45a085fb683718730afdce4ffdcfc3cba3d40
Siddhesh Gawade <siddheshgawade61@gmail.com>
2024-06-25 16:11:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  let [data , setData] = useState([]);
+  let [filteredData, setFilteredData] = useState([]);
+  let [currentPage, setCurrentPage] = useState(1);
+
+  let nextPage = ()=>{
+    setCurrentPage((prev)=> (((prev+1)*10)-10)<data.length?prev+1:prev);
+  };
+  let prevPage = ()=>{
+    setCurrentPage((prev)=> prev>1?prev-1:1);
+  };
+
+  function fillFiltersData(){
+    let newArr = [];
+    for(let i=(currentPage*10)-10;i<(currentPage*10);i++){
+      if(i>=data.length) break;
+      newArr.push(data[i]);
+    }
+    setFilteredData(newArr);
+  }
+  useEffect(()=>{
+    fillFiltersData();
+  },[currentPage, data]);
+
+  useEffect(()=>{
+    fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      setData(data)
+    })
+    .catch((err)=>{console.alert("failed to fetch data")});
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee Data Table</h1>
+      <table className="empTable" style={{width:"100vw",height:'70vh'}}>
+        <tr style={{backgroundColor:"green",color:"white"}}>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+        {filteredData.map((fdata, index)=>(
+          <tr key={index}>
+            <td>{fdata.id}</td>
+            <td>{fdata.name}</td>
+            <td>{fdata.email}</td>
+            <td>{fdata.role}</td>
+          </tr>
+        ))}
+
+      </table>
+      <div style={{display:"flex",justifyContent:"center", gap:'8px'}} className="buttons">
+        <button onClick={prevPage}>Previous</button>
+        <div className="currentPage">{currentPage}</div>
+        <button onClick={nextPage}>Next</button>
+      </div>
     </div>
   );
 }
